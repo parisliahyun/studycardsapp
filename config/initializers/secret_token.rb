bc45def54c6aa547f5e4a265544ee703cd808c35
My Name <parisliahyun@gmail.com>
2015-11-01 15:42:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudyCards::Application.config.secret_key_base = '935865190119c17ab001d7307541c4bdb0460b5550d72f04b0b8c93927dccaa0ec8258e0d62e863eeac3623451f282d7ada12e850c69fef8d92de7d4925f1250'

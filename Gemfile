7d836d5870b3d6d30e474e05b9196c9063a28660
My Name <parisliahyun@gmail.com>
2015-11-01 16:20:17 -0800

    add bootstrap

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '4.0.4'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 
+gem 'sass', '~> 3.2.2'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
 
@@ -39,6 +41,8 @@ end
 group :development, :test do
   gem 'sqlite3'
 end
+
+gem 'bootstrap-sass', '~> 3.0.3.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 StudyCards::Application.routes.draw do
+  root :to => 'pages#home'
+  get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
